e6c95d3783d6935de459e1e88c9325dd7e564d6d
Arseniy Khvorov <khvorov45@gmail.com>
2021-07-15 14:44:32 +1000

    start changing to a single-file use

--- replace/src/main.rs
+++ replace/src/main.rs
@@ -1,35 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input_dir = ".";
-    let (mut pages, components) = {
-        let mut pages = Vec::new();
-        let mut components = Vec::new();
-        for entry in std::fs::read_dir(input_dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_file() {
-                let name = entry.file_name();
-                let name = name.to_str().unwrap();
-                let contents = std::fs::read_to_string(&path)?;
-                if name.ends_with(".html") {
-                    let name = name.replace(".html", "");
-                    if name.starts_with(|c: char| c.is_uppercase()) {
-                        components.push(Html {
-                            name,
-                            path,
-                            contents: contents.trim_end().to_string(),
-                        });
-                    } else {
-                        pages.push(Html {
-                            name,
-                            path,
-                            contents,
-                        })
-                    }
-                }
-            }
-        }
-        (pages, components)
-    };
+    let input_dir = std::path::PathBuf::from(".");
+    let input_file_name = "index.html";
+
+    let input_file = input_dir.join(input_file_name);
+    let input_contents = std::fs::read_to_string(input_file).unwrap();
+
+    let output_contents = input_contents.clone();
+
+    let mut input_to_parse = input_contents.as_str();
+    while let Some(substitution) = Substitution::find_first(&mut input_to_parse) {}
 
     let output_dir = std::path::PathBuf::from("build");
     if output_dir.is_dir() {
@@ -37,28 +16,92 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     std::fs::create_dir(output_dir.as_path())?;
 
-    for page in &mut pages {
-        for component in &components {
-            let pattern = format!("<{} />", component.name);
-            if page.contents.contains(pattern.as_str()) {
-                page.contents = page
-                    .contents
-                    .replace(pattern.as_str(), component.contents.as_str());
-            }
-        }
-        let old_tree =
-            page.path
-                .strip_prefix(format!("{}{}", input_dir, std::path::MAIN_SEPARATOR))?;
-        let new_path = output_dir.join(old_tree);
-        std::fs::write(new_path, page.contents.as_bytes())?;
-    }
+    let output_file = output_dir.join(input_file_name);
+
+    std::fs::write(output_file, output_contents.as_bytes())?;
+
+    println!("Done");
 
     Ok(())
 }
 
 #[derive(Debug)]
-struct Html {
-    name: String,
-    path: std::path::PathBuf,
-    contents: String,
+struct Substitution {
+    literal: String,
+    component_name: String,
+}
+
+impl Substitution {
+    fn find_first(string: &mut &str) -> Option<Self> {
+        fn find_start(string: &str) -> &str {
+            // NOTE(sen) Does not handle nodes inside comments
+            let mut start_index = string.len();
+            if !string.is_empty() {
+                for string_index in 0..(string.len() - 1) {
+                    if string.chars().nth(string_index) == Some('<')
+                        && string.chars().nth(string_index + 1) != Some('!')
+                    {
+                        start_index = string_index;
+                        break;
+                    }
+                }
+            }
+            &string[start_index..]
+        }
+
+        let mut node = None;
+        let mut string = find_start(string);
+
+        if !string.is_empty() {
+            fn find_name(string: &mut &str) -> String {
+                debug_assert!(string.starts_with('<'));
+                let string_to_parse = string[1..].trim_start();
+                let name_break = string_to_parse
+                    .find(|x: char| !x.is_alphanumeric())
+                    .unwrap();
+                let (name, left) = string_to_parse.split_at(name_break);
+                *string = left;
+                name.to_string()
+            }
+
+            let name = find_name(&mut string);
+
+            fn find_params(string: &mut &str) -> std::collections::HashMap<String, String> {
+                let string_to_parse = string.trim_start();
+                let params_break = string_to_parse
+                    .find(|x: char| x == '/' || x == '>')
+                    .unwrap();
+                let (params_string, left) = string_to_parse.split_at(params_break);
+
+                let mut params = std::collections::HashMap::new();
+
+                let params_string_to_parse = params_string.trim_end();
+                if !params_string_to_parse.is_empty() {}
+
+                params
+            }
+
+            let params = find_params(&mut string);
+
+            fn find_children(string: &mut &str) -> Vec<Substitution> {
+                let children = Vec::new();
+
+                if let Some(stripped) = string.strip_prefix("/>") {
+                    *string = stripped;
+                } else if let Some(stripped) = string.strip_prefix(">") {
+                    *string = stripped;
+                } else {
+                    panic!("unexpected string");
+                }
+
+                children
+            }
+
+            let children = find_children(&mut string);
+
+            string = &string[string.len()..];
+        }
+
+        node
+    }
 }
