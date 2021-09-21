c2d3c146d2a1df270132ef578f6079b5050b567e
Arseniy Khvorov <khvorov45@gmail.com>
2021-09-21 14:44:46 +1000

    update linux build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-ODIN_FLAGS_COMMON="-out:out/htmlrc -default-to-nil-allocator -no-crt -verbose-errors"
+ODIN_FLAGS_COMMON="-out:out/htmlrc -verbose-errors"
 ODIN_FLAGS_DEBUG_OR_RELEASE="-debug"
 if [ "$1" == "release" ]
   then
     ODIN_FLAGS_DEBUG_OR_RELEASE="-o:speed"
 fi
 
-odin build htmlrc $ODIN_FLAGS_COMMON $ODIN_FLAGS_DEBUG_OR_RELEASE
+odin build code/htmlrc.odin $ODIN_FLAGS_COMMON $ODIN_FLAGS_DEBUG_OR_RELEASE
 
 echo done
